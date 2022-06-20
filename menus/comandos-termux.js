0fef2475f3273243d6b5df12da05adf9c84b3008
blabla <blablablabla@gmail.com>
2022-06-20 11:41:31 -0300

    Atualizando/Corrigindo

--- menus/comandos-termux.js
+++ menus/comandos-termux.js
@@ -8,34 +8,87 @@ SÓ DEVO LEMBRAR QUE O TERMUX DA PLAY STORE NÃO PRESTA, ENTÃO INSTALE O 118 PO
 Comandos Básicos do termux, nescessario para inicialização do bot:
 
 
--_1 COMANDO :
+- _1 COMANDO :
 
-termux-change-repo 
 
-Confirma, marca a terceira caixinha e confirma e prossegue > 
+termux-change-repo
 
--_2 COMANDO :
+APERTA OK
 
-apt-get upgrade
+_-_-_       _-_-_.        _-_-_
 
-Vai precisar digitar y e confirmar toda vez que pedir.
+MARCA A TERCEIRA CAIXINHA E APERTA OK.
 
--_3 COMANDO :
+_-_-_      _-_-_-        _-_-_-
 
-apt-get update
 
-Vai precisar digitar y e confirmar toda vez que pedir.
+_2 COMANDO
 
--_4 Comando :
 
-pkg upgrade -y && pkg update -y && pkg install nodejs -y && pkg install nodejs-lts -y && pkg install ffmpeg -y && pkg install wget -y && pkg install git -y
+pkg upgrade -y 
 
--_5 COMANDO :
+
+ASSISTA O VIDEO, ALGUMAS VEZES QUANDO PAUSAR É NESCESSARIO DA ENTER, VAI PAUSAR A AÇÃO E VAI APARECER [default=N] ?
+
+_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+_3 COMANDO 
+
+
+pkg update -y
+
+
+_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+_4 COMANDO
+
 
 termux-setup-storage
 
+_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 
-E permite.
+_5 COMANDO 
+
+
+pkg install nodejs -y
+
+_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+_6 COMANDO 
+
+
+pkg install nodejs-lts -y
+
+_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+_7 COMANDO 
+
+
+pkg install git -y
+
+_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+
+_8 COMANDO 
+
+
+pkg install ffmpeg -y
+
+_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+
+_9 COMANDO NESCESSARIO PRA O HEROKU
+
+
+npm i -g npm@6 && npm i heroku -g
+
+
+_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+
+PRONTO, BOA SORTE... 
+
+
 `
 }
 
