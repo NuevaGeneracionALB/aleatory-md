8b01787f7eb74bb94f7f9cbfe9ccb278ea510266
blabla <blablablabla@gmail.com>
2022-08-21 10:03:17 -0300

    Atualizando

--- menus/menus.js
+++ menus/menus.js
@@ -109,7 +109,7 @@ exports.menu = menu
 
 // MENU DE ADMINISTRADORES 
 
-const adms = (prefix) => { 
+const adms = (prefix, sender) => { 
  
 // NÃO APAGUE ESSE ${prefix}, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa. 
 
