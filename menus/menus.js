6dd4d2c51022f526fca24d9c21a428c5bcefd0ec
Your Name <you@example.com>
2022-10-25 09:53:59 -0300

    CORRIGINDO

--- menus/menus.js
+++ menus/menus.js
@@ -119,6 +119,41 @@ exports.menu = menu
 // NÃO APAGUE ESSE ${NickDono} nem 
 //${numerodn} nem ${NomeDoBot} nem ${prefix} só se quiser apagar completo, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa.
 
+const anotacao = (prefix) => {
+return `
+
+Olá. Se você está lendo isso, provavelmente está curioso sobre o comando anotação. 
+
+Existe os seguintes comando:
+
+1 ${prefix}anotações
+
+2 ${prefix}anotar
+
+3 ${prefix}tirar_nota
+
+4 ${prefix}nota titulo
+
+
+O primeiro ele mostra todas as anotações do grupo, criada. 
+
+O segundo, ele é usado para criar a nota, tipo um bloco de notas, exemplo: ${prefix}anotar ABC|ABC são 3 letras do alfabeto, utilizada bastante
+
+Isso foi um exemplo, mas pode ser utilizado da fórma que quiser, mas lembre que antes do | é o título, depois é a anotação.
+
+Ja o terceiro, é pra tirar a nota, Exemplo como expliquei, ${prefix}tirar_nota ABC 
+
+Com isso a anotação que estava criada foi apagada. 
+
+Já o terceiro, ele é pra buscar a anotação que deseja, pelo título, exemplo: ${prefix}nota ABC
+
+É isso... 
+
+Boa sorte. 
+`
+}
+
+exports.anotacao = anotacao
 
 // MENU DE ADMINISTRADORES 
 
