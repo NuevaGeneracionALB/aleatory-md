3535efb16c8d2ca053ac237d06b484540e1a191c
Your Name <you@example.com>
2022-11-01 06:33:40 -0300

    ATUALIZANDO-CORRIGINDO

--- armor/js/infocontador.js
+++ armor/js/infocontador.js
@@ -37,6 +37,15 @@ contém alguns comandos
 Exemplo: ${prefix}banghost 0 
 
 ele vai banir todos aqueles que tiver com 0 mensagem, mas não digite este comando fora do termux (EXEMPLO: HOSPEDADO EM SITES, NÃO NO TERMUX, COMO FOSSE COMANDO, O COMANDO É PRA SER EXECUTADO NO WHATSAPP) , pois ele vai remover todos sem parar, que tiver 0 mensagem, já no termux, ele bane uma pessoa por comando, leia com atenção isso, pois não irei ser responsável por remover todos do seu grupo, por seus erros, recomendo deixar o bot on por 1 semana no grupo, pra usar esse comando, e banir aqueles que nunca enviou mensagem no grupo.)
+
+
+(5) ${prefix}inativos 
+
+
+Esse, ele vai te entregar os membros que estão com x mensagens definida por você, ou abaixo daquilo, exemplo: ${prefix}inativos 2
+Todos com 2 mensagens ou menos, serão mostrado na lista... 
+
+Má lembre, se colocou o bot agora, todos estarão zerado, pois não armazenou informações suficiente.
  `
 }
 
