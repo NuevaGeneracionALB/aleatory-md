a6f0a1be363d2316adb1b9ce6b3c73449cd68065
Aleatory Conteúdos <aleatory@gmail.com>
2023-05-19 16:26:51 -0300

    CORREÇÃO

--- start.sh
+++ start.sh
@@ -1,8 +1,25 @@
-#!bin/bash
+#!/bin/bash
+
+# Definindo cores
 GREEN='\033[0;32m'
-while : 
-do
-echo "${GREEN} Aleatory - Auto reconexão ativada para prevenção de quedas.."
-node iniciar.js
-sleep 1
+
+# Definindo o fuso horário para o Brasil
+export TZ=America/Sao_Paulo
+
+# Loop infinito
+while true; do
+    echo -e "${GREEN}Reconectando Aleatory 3.6 / Iniciando - $(date +'%d/%m/%Y %H:%M:%S')"
+    
+    # Executando o script Node.js
+    node conect.js
+    
+    # Verificando o código de saída
+    if [[ $? -eq 0 ]]; then
+        echo -e "${GREEN}Script concluído com sucesso."
+    else
+        echo -e "${GREEN}Houve um erro ao executar o script."
+    fi
+    
+    # Aguardando 1 segundo antes de reiniciar
+    sleep 1
 done
