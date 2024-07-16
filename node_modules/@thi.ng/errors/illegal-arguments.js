ad1adc17f961901efc222683a56d9b2fd61272df
Aleatory <josivalribeiro@gmail.com>
2024-07-16 13:37:47 -0300

    Correção.

--- node_modules/@thi.ng/errors/illegal-arguments.js
+++
@@ -1,9 +0,0 @@
-import { defError } from "./deferror.js";
-const IllegalArgumentError = defError(() => "illegal argument(s)");
-const illegalArgs = (msg) => {
-  throw new IllegalArgumentError(msg);
-};
-export {
-  IllegalArgumentError,
-  illegalArgs
-};
--- node_modules/@thi.ng/errors/illegal-arity.js
+++
@@ -1,9 +0,0 @@
-import { defError } from "./deferror.js";
-const IllegalArityError = defError(() => "illegal arity");
-const illegalArity = (n) => {
-  throw new IllegalArityError(n);
-};
-export {
-  IllegalArityError,
-  illegalArity
-};
